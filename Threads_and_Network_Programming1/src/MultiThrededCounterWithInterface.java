7d2ebc8b77c1472755daa7badeb8a9139721a9b1
thekocanxsec <imran.kocan@gmail.com>
2026-08-27 12:19:00 +0200

    finished

--- Threads_and_Network_Programming1/src/MultiThrededCounterWithInterface.java
+++ Threads_and_Network_Programming1/src/MultiThrededCounterWithInterface.java
@@ -1,2 +1,32 @@
 public class MultiThrededCounterWithInterface implements Runnable{
+    private int threadNumber;
+
+    public MultiThrededCounterWithInterface(int threadNumber){
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run(){
+        for(int i = 0; i <= 5; i++){
+            try{
+                System.out.println(i + " from the thread: " + this.threadNumber);
+                Thread.sleep(100);
+            }catch(InterruptedException e){
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class MainRun3{
+    static void main(String[] args) {
+        MultiThrededCounterWithInterface counter = new MultiThrededCounterWithInterface(2);
+        Thread thread = new Thread(counter);
+
+        MultiThrededCounterWithInterface counter2 = new MultiThrededCounterWithInterface(1);
+        Thread thread2 = new Thread(counter2);
+
+        thread.start();
+        thread2.start();
+    }
 }
